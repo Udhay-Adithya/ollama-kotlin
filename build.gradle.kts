a87afe2b0330651d7d8dcb4544b4db93892f6c15
Udhay-Adithya <udhayxd@gmail.com>
2026-04-29 22:29:59 +0530

    chore: update license from Apache License 2.0 to MIT

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,8 +22,8 @@ mavenPublishing {
         url.set("https://github.com/Udhay-Adithya/ollama-kotlin")
         licenses {
             license {
-                name.set("The Apache License, Version 2.0")
-                url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
+                name.set("The MIT License")
+                url.set("https://opensource.org/licenses/MIT")
                 distribution.set("repo")
             }
         }
